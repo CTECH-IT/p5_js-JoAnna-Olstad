d09cf0d40b755561d65f952f5a914ec084e40c21
JoAnna Olstad <5638402@isd535.org>
2022-05-20 11:20:16 -0500

    Moved the code from p5.js editor to VS code, Added description

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=7779101&assignment_repo_type=AssignmentRepo)
-# JS-Template
-This is a repository you can use to start your HTML/CSS/JavaScript project.
 
-I have included: index.html, styles.css, main.js, and bs.bat - the batch file that will start browser-sync automatically
-
-There is also a .gitignore which will cause GitHub to ignore a bunch of files that shouldn't be stored (like temp files, log files, node libraries, etc)
-
-### Make sure you edit *this file* (REAMDE.md), delete all this information and put in your name and a short description of this project.
-
-Then, edit index.html and add your name in the author meta field. 
-
-Finally, commit and push to make sure it's working for you.
+JoAnna Olstad
+I am going to make an American flag and put the text of the pledge of allegiance over the flag
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,112 @@
-// Put your JavaScript here
+function setup() {
+    createCanvas(1000, windowWidth);
+  
+  }
+  
+  function draw() {
+    background(500);
+    
+    //Red Stripes
+    rect(0, 900,windowWidth, 50);
+    fill(255,0,0)
+    rect(0, 700,windowWidth, 50);
+    fill(255,0,0)
+    rect(0, 600,windowWidth, 50);
+    fill(255,0,0)
+    rect(0, 500,windowWidth, 50);
+    fill(255,0,0)
+    rect(0, 400,windowWidth, 50);
+    fill(255,0,0)
+    rect(0, 300,windowWidth, 50);
+    fill(255,0,0)
+    
+    fill(255,0,0)
+    rect(250, 0,windowWidth*.68, 50);
+    fill(255,0,0)
+    rect(250, 200,windowWidth*.68, 50);
+    fill(255,0,0)
+    rect(250, 100,windowWidth*.68, 50 );
+    fill(255,0,0)
+    
+    //Blue Box
+    fill(9,0,255)
+    rect(0,0,533,300)
+  
+    //Yellow "Star"
+    fill(128,128,128)
+    //Row 1
+    rect(0,0,41,33)
+    rect(82,0,41,33)
+    rect(164,0,41,33)
+    rect(246,0,41,33)
+    rect(328,0,41,33)
+    rect(410,0,41,33)
+    rect(492,0,41,33)
+    //Row 2
+    rect(41,33,41,33)
+    rect(123,33,41,33)
+    rect(205,33,41,33)
+    rect(287,33,41,33)
+    rect(369,33,41,33)
+    rect(451,33,41,33)
+    //Row 3
+    rect(0,66,41,33)
+    rect(82, 66,41,33)
+    rect(164,66,41,33)
+    rect(246,66,41,33)
+    rect(328,66,41,33)
+    rect(410,66,41,33)
+    rect(492,66,41,33)
+    //Row 4
+    rect(41,99,41,33)
+    rect(123,99,41,33)
+    rect(205,99,41,33)
+    rect(287,99,41,33)
+    rect(369,99,41,33)
+    rect(451,99,41,33)
+    //Row 5
+    rect(0,132,41,33)
+    rect(82, 132,41,33)
+    rect(164,132,41,33)
+    rect(246,132,41,33)
+    rect(328,132,41,33)
+    rect(410,132,41,33)
+    rect(492,132,41,33)
+    //Row 6
+    rect(41,165,41,33)
+    rect(123,165,41,33)
+    rect(205,165,41,33)
+    rect(287,165,41,33)
+    rect(369,165,41,33)
+    rect(451,165,41,33)
+    //Row 7
+    rect(0,198,41,33)
+    rect(82, 198,41,33)
+    rect(164,198,41,33)
+    rect(246,198,41,33)
+    rect(328,198,41,33)
+    rect(410,198,41,33)
+    rect(492,198,41,33)
+    //Row 8
+    rect(41,231,41,33)
+    rect(123,231,41,33)
+    rect(205,231,41,33)
+    rect(287,231,41,33)
+    rect(369,231,41,33)
+    rect(451,231,41,33)
+    //Row 9
+    rect(0,264,41,33)
+    rect(82,264,41,33)
+    rect(164,264,41,33)
+    rect(246,264,41,33)
+    rect(328,264,41,33)
+    rect(410,264,41,33)
+    rect(492,264,41,33)
+    
+    //Text
+      textSize(50);
+    fill(10,10,10);
+    textWrap(WORD);
+    text('"I pledge allegiance to the Flag of the United States of America, and to the Republic for which it stands, one Nation under God, indivisible, with liberty and justice for all."',100, 300,700);
+  }
+  
